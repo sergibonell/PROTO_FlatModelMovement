0932673e9d1a44a5ec82ccd15e379d2877f877ae
Sergi <sergibonell@gmail.com>
2022-11-15 18:13:38 +0100

    Implemented highlight

--- Assets/Scripts/ChestLogic.cs
+++ Assets/Scripts/ChestLogic.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IInteractuable : MonoBehaviour
+public class ChestLogic : MonoBehaviour, IInteractable
 {
+    public void OnInteract()
+    {
+        Debug.Log("Activated chest: " + gameObject.name);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/InteractionHandler.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectedObject;
+    private IInteractable inter;
+
+    private int defaultLay;
+    private int highlightLay;
+
+    private void Awake()
+    {
+        defaultLay = LayerMask.NameToLayer("Default");
+        highlightLay = LayerMask.NameToLayer("Highlighted");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<IInteractable>(out inter) && selectedObject == null)
+        {
+            selectedObject = other.gameObject;
+            changeLayer(highlightLay);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == selectedObject)
+        {
+            changeLayer(defaultLay);
+            selectedObject = null;
+        }
+    }
+
+    private void changeLayer(int layer)
+    {
+        if (selectedObject == null)
+        {
+            Debug.LogWarning("Can't highlight null object"); 
+            return;
+        }
+
+        selectedObject.layer = layer;
+        foreach(Transform child in selectedObject.transform)
+            child.gameObject.layer = layer;
+    }
+}
